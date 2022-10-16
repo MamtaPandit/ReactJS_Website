8f726c65d9fbcf012a9d9df4fee9ab96e7d9c766
ddoshi983003 <115929363+ddoshi983003@users.noreply.github.com>
2022-10-16 17:24:14 +0530

    React js code for fragments added

---
+++ ReactJS_Website/src/fragments.jsx
@@ -0,0 +1,11 @@
+// Rendering with fragments tag  
+class App extends React.Component {   
+    render() {   
+     return (   
+       <React.Fragment>  
+            <h2> Hello World! </h2>   
+        <p> Welcome to the JavaTpoint. </p>   
+         </React.Fragment>  
+     );   
+    }   
+}   
